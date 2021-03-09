3cf7196249fecc1622e55d69a9b32387da7e3d5b
Jibran <jibraniqbal@protonmail.com>
2021-03-10 00:40:48 +0500

    autoformatting

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,45 +16,45 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androiddevchallenge.ui.theme.MyTheme
-import android.os.CountDownTimer
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.material.Icon
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PauseCircleOutline
 import androidx.compose.material.icons.outlined.PlayCircleOutline
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.ui.theme.MyTheme
 
 var totalSeconds = 1500
 
@@ -127,7 +127,6 @@ fun PomodoroScreen() {
             }
         }
     }
-
 }
 
 @Composable
@@ -151,7 +150,7 @@ fun PlayPauseButton(isPlaying: Boolean, onClick: () -> Unit) {
 @Composable
 fun PomodoroTimer(secondsLeft: Int) {
     Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-        ProgressDial(secondsLeft * 100 / totalSeconds )
+        ProgressDial(secondsLeft * 100 / totalSeconds)
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
                 "${secondsLeft / 60}",
@@ -165,7 +164,6 @@ fun PomodoroTimer(secondsLeft: Int) {
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold
             )
-
         }
     }
 }
@@ -182,17 +180,20 @@ fun ProgressDial(progress: Int) {
 
 @Composable
 fun Donut(color: Color, sweepAngle: Float) {
-    Canvas(modifier = Modifier
-        .fillMaxWidth(0.6f)
-        .then(Modifier.aspectRatio(1f)), onDraw = {
-        drawArc(
-            color,
-            startAngle = 270f,
-            sweepAngle = sweepAngle,
-            false,
-            style = Stroke(this.size.width / 16, cap = StrokeCap.Round)
-        )
-    })
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth(0.6f)
+            .then(Modifier.aspectRatio(1f)),
+        onDraw = {
+            drawArc(
+                color,
+                startAngle = 270f,
+                sweepAngle = sweepAngle,
+                false,
+                style = Stroke(this.size.width / 16, cap = StrokeCap.Round)
+            )
+        }
+    )
 }
 
 val primaryColor = Color(248, 89, 89)
